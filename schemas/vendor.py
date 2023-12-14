93a365b805ff2dde01ce86da38a13b48a7def193
Edward Tay <130837452+EdwardBKTay@users.noreply.github.com>
2023-12-14 23:02:59 +0800

    minor changes

--- schemas/vendor.py
+++ schemas/vendor.py
@@ -1,7 +1,4 @@
-from fastapi import HTTPException, status
-from pydantic import BaseModel, Field, EmailStr, SecretStr, field_validator, field_serializer
 from schemas.user import UserCreate
-import re
 
 class VendorCreate(UserCreate):
     pass
