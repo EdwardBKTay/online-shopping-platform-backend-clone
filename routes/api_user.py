8f8bd2948a970064b532491f1da3b32401e6f2bc
BK <130837452+EdwardBKTay@users.noreply.github.com>
2023-12-16 11:14:47 +0800

    rewrite endpoints

--- routes/api_user.py
+++ routes/api_user.py
@@ -1,8 +1,12 @@
+import datetime
 from typing import Annotated
 from fastapi import APIRouter, Form, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import EmailStr, SecretStr, ValidationError
 from sqlmodel import Session
+from auth.auth import ACCESS_TOKEN_EXPIRATION_MINUTES, create_access_token
 from db.models import User
+from schemas.token import TokenPayload, Token
 
 from schemas.user import UserCreate
 from crud.crud_user import user
@@ -11,7 +15,7 @@ from core.deps import get_session, get_current_entity
 users_router = APIRouter()
 
 # TODO: route for profile of users (GET)
-@users_router.get("/", dependencies=[Depends(get_current_entity)])
+@users_router.get("/me")
 async def get_user(session: Annotated[Session, Depends(get_session)], current_user: Annotated[User, Depends(get_current_entity)]):
     return user.get(session, current_user.username)
 
@@ -23,3 +27,17 @@ async def create_user(username: Annotated[str, Form()], email: Annotated[EmailSt
         return {"message": "User has been created"}
     except ValidationError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Validation Error: {e}") from e
+
+@users_router.post("/token/")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], session: Annotated[Session, Depends(get_session)]):
+    http_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid user credentials")
+    
+    user_obj = user.authenticate(session, form_data.username, form_data.password)
+    
+    if user_obj is None:
+        raise http_exception
+
+    token_payload = TokenPayload(username=user_obj.username, email=user_obj.email, exp=datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRATION_MINUTES))
+
+    access_token = create_access_token(token_payload)
+    return Token(access_token=access_token, token_type="bearer")
--- routes/api_vendor.py
+++ routes/api_vendor.py
@@ -1,9 +1,13 @@
+import datetime
 from typing import Annotated
 from fastapi import APIRouter, Depends, Form, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session
+from auth.auth import ACCESS_TOKEN_EXPIRATION_MINUTES, create_access_token
 from crud.crud_vendor import vendor
 
 from core.deps import get_current_entity, get_session
+from schemas.token import TokenPayload, Token
 from schemas.vendor import VendorCreate
 from pydantic import EmailStr, SecretStr, ValidationError
 
@@ -22,3 +26,17 @@ async def create_vendor(username: Annotated[str, Form()], email: Annotated[Email
         return {"message": "Vendor has been created"}
     except ValidationError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Validation Error: {e}") from e
+
+@vendor_router.post("/token/")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], session: Annotated[Session, Depends(get_session)]):
+    http_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid vendor credentials")
+    
+    vendor_obj = vendor.authenticate(session, form_data.username, form_data.password)
+    
+    if vendor_obj is None:
+        raise http_exception
+
+    token_payload = TokenPayload(username=vendor_obj.username, email=vendor_obj.email, exp=datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRATION_MINUTES))
+
+    access_token = create_access_token(token_payload)
+    return Token(access_token=access_token, token_type="bearer")
